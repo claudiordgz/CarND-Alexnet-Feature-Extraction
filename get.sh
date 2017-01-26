75f912093aeec7f858f6f99441379fd186d2fd3a
Claudio Rodriguez <claudio.rdgz@gmail.com>
2017-01-26 18:44:06 -0500

    feat: retrieve datasets

---
+++ get.sh
@@ -0,0 +1,3 @@
+#!/bin/bash          
+WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580a829f_train/train.p
+WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580d880c_bvlc-alexnet/bvlc-alexnet.npy
