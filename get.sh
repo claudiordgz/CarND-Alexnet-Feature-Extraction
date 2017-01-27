ffa659b65f22ef7b594d6cb07272b77e1ec0d1c1
Claudio Rodriguez <claudio.rdgz@gmail.com>
2017-01-26 23:09:00 -0500

    notes: wrapped alexnet

--- get.sh
+++ get.sh
@@ -1,3 +1,7 @@
 #!/bin/bash          
 WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580a829f_train/train.p
 WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/October/580d880c_bvlc-alexnet/bvlc-alexnet.npy
+WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b432_vgg-100/vgg-100.zip
+WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b634_resnet-100/resnet-100.zip
+WGET https://d17h27t6h515a5.cloudfront.net/topher/2016/November/5834b498_inception-100/inception-100.zip
+find . -name '*.zip' -exec sh -c 'unzip -d "${1%.*}" "$1"' _ {} \;
